6a911d858bb04ef6b068a29c9ea15cec8c0d8133
aniqlashari <mahaqamber3@gmail.com>
2024-09-26 20:45:47 +0500

    first commit

---
+++ style.js
@@ -0,0 +1,23 @@
+alert ("Yoo man Come In My Website Now Playing A Quiz")
+let score = 0
+let name = prompt("Enter  world reahest man Name ?")
+let name2 = prompt("name beast old english singer ?")
+let name3 = prompt("how many area holding by queen elizabeth ?")
+let name4 = prompt("what is the name of first man on moon ?")
+if (name === "elonmusk"){
+score++
+}
+else if (name2 === "michaljakson") {
+    score++
+}
+
+else if(name3==="32"){
+score++
+}
+else if(name4==="Neil Armstrong"){
+score++
+
+}
+
+alert("your score is"+score)
+
